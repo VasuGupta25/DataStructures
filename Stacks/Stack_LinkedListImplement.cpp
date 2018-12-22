e80c14b5ea808a767c18c40feed83d9e9b8e6bcc
Vasu Gupta <45697639+VasuGupta25@users.noreply.github.com>
2018-12-23 01:33:55 +0530

    Create Stack_LinkedListImplement.cpp

---
+++ Stacks/Stack_LinkedListImplement.cpp
@@ -0,0 +1,53 @@
+// #Lect 16
+// Implementing Stck using Linked List
+
+
+#include <iostream>
+using namespace std;
+struct Node
+{
+	int data;
+	Node *next;
+};
+
+Node *top=NULL;
+
+void push(int x)
+{
+	Node *temp= new Node();
+	temp->data=x;
+	temp->next=top;
+	top=temp;
+}
+
+void pop()
+{
+	Node* temp;
+	if(top==NULL)
+	{
+		return;
+	}
+	temp=top;
+	top=top->next;
+	delete temp;
+}
+
+void display()
+{
+	Node *temp;
+	temp=top;
+	while(temp!=NULL)
+	{
+		cout<<temp->data;
+		temp=temp->next;
+	}
+}
+int main()
+{
+	push(2);   //pushing elements to stack
+	push(5);
+	push(9);
+	pop();     // pop the top element from stack
+	display();  //display the whole stack
+	return 0;
+}
